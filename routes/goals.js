5136a892af17c7fee7dcfbfcb2dea0171dcd7887
Kamran Elchuzade <elchuzade@gmail.com>
2020-05-23 17:38:01 +0300

    added update goal route

--- routes/goals.js
+++ routes/goals.js
@@ -36,12 +36,13 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
-    // Get the parent goal
-    const parent = await Goal.findById(req.params.id)
-
-    const { title, text, deadline, repeat } = req.body
 
     try {
+      // Get the parent goal
+      const parent = await Goal.findById(req.params.id)
+
+      const { title, text, deadline, repeat } = req.body
+
       const newGoal = new Goal({
         title,
         text,
@@ -56,6 +57,41 @@ router.post(
       await parent.save()
 
       return res.status(200).json(parent)
+    } catch (err) {
+      console.error(err.message)
+      return res.status(500).send('Server Error')
+    }
+  }
+)
+
+module.exports = router
+
+// @route   POST api/goals/:id
+// @desc    Update a goal
+// @access  Public
+router.put(
+  '/:id',
+  [auth, [check('title', 'Please include a goal title').exists()]],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+      // Get the parent goal
+      let goal = await Goal.findById(req.params.id)
+
+      const { title, text, deadline, repeat } = req.body
+
+      if (title) goal.title = title
+      if (text) goal.text = text
+      if (deadline) goal.deadline = deadline
+      if (repeat) goal.repeat = repeat
+
+      goal = await goal.save()
+
+      return res.status(200).json(goal)
     } catch (err) {
       console.error(err.message)
       return res.status(500).send('Server Error')

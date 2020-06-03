6258810e04a7750fa8e2d9725c652850c4519a27
Kamran Elchuzade <elchuzade@gmail.com>
2020-06-03 15:28:35 +0300

    added grandchildren lines

--- client/src/pages/bubble/Lines.js
+++ client/src/pages/bubble/Lines.js
@@ -4,38 +4,45 @@ import { Line } from 'react-lineto';
 import { PercentToPixel } from '../../utils/convertPixelPercent'
 
 const Lines = ({ plainDims, goal }) => {
-  return (
-    <Fragment>
-      {goal.children && goal.children.map((child, index) =>
-        <Line
-          zIndex={-1}
-          key={index}
-          x0={
-            plainDims.leftOffset +
-            PercentToPixel(
-              goal.position.x,
-              plainDims.plainWidth
-            )
-          }
-          y0={
-            plainDims.topOffset +
-            PercentToPixel(
-              goal.position.y,
-              plainDims.plainHeight
-            )
-          }
-          x1={
-            plainDims.leftOffset +
-            PercentToPixel(child.position.x, plainDims.plainWidth)
-          }
-          y1={
-            plainDims.topOffset +
-            PercentToPixel(child.position.y, plainDims.plainHeight)
-          }
-        />
-      )}
-    </Fragment>
+  const DrawLine = (parent, child, index) => (
+    <Line
+      zIndex={-1}
+      key={index}
+      x0={
+        plainDims.leftOffset +
+        PercentToPixel(
+          parent.position.x,
+          plainDims.plainWidth
+        )
+      }
+      y0={
+        plainDims.topOffset +
+        PercentToPixel(
+          parent.position.y,
+          plainDims.plainHeight
+        )
+      }
+      x1={
+        plainDims.leftOffset +
+        PercentToPixel(child.position.x, plainDims.plainWidth)
+      }
+      y1={
+        plainDims.topOffset +
+        PercentToPixel(child.position.y, plainDims.plainHeight)
+      }
+    />
   )
+
+  return <Fragment>
+    {goal.children && goal.children.map((child, index) =>
+      (<>
+        {DrawLine(goal, child, index)}
+        {child.children.map((grandChild, grandIndex) => (
+          DrawLine(child, grandChild, grandIndex)
+        ))}
+      </>)
+    )}
+  </Fragment>
 }
 
 export default Lines
